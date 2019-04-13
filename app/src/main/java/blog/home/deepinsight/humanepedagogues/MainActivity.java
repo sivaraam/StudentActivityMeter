2ea2cc0ab2f488e95df83da889a10f883665ac47
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2019-04-13 20:12:00 +0530

    Use fragments with dummy list of items for the navigation bar items
    We think of having a list of students for both the navigation bar items.
    So, we just try to do the same with a dummy list fragment.

--- app/src/main/java/blog/home/deepinsight/humanepedagogues/MainActivity.java
+++ app/src/main/java/blog/home/deepinsight/humanepedagogues/MainActivity.java
@@ -4,27 +4,51 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import blog.home.deepinsight.humanepedagogues.dummy.DummyContent;
+
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StudentListFragment.OnListFragmentInteractionListener, AspirantsFragment.OnListFragmentInteractionListener {
 
     private TextView mTextMessage;
+
+    /*
+     * Taken from a blog titled: Bottom Navigation Android Example using Fragments
+     *
+     * https://www.simplifiedcoding.net/bottom-navigation-android-example/
+     *
+     */
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    fragment = new StudentListFragment();
+                    break;
                 case R.id.navigation_aspirants:
-                    mTextMessage.setText(R.string.title_aspirants);
-                    return true;
+                    fragment = new AspirantsFragment();
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
 
@@ -33,9 +57,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        /*
+         * By default load the Student list fragment.
+         */
+        loadFragment(new StudentListFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item)
+    {
+        Toast.makeText(MainActivity.this, (String)item.details,
+                Toast.LENGTH_SHORT).show();
     }
 
 }
